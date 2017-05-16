bbc2aee16eb8e25db4713fc1a54035c84842a113
Adam Jankowski <adam.jankowski@protonmail.com>
2017-05-16 09:10:54 +0200

    Not working authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,16 @@
 class SessionsController < ApplicationController
-    protect_from_forgery
+
     def new
+        if(current_user != nil)
+            redirect_to '/'
+        end
     end
     
     def create
         user = User.find_by_email(params[:user][:email])
         if user && user.authenticate(params[:user][:password])
             session[:id] = user.id
+            session[:message] = "ajksfn aksjf ja sfjk"
             redirect_to '/'
         else
             redirect_to '/login'
