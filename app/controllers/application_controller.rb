e03d9f77865d883989aa036507f94ba88e67fa14
Adam Jankowski <adam.jankowski@protonmail.com>
2017-05-17 21:19:10 +0200

    Fixed signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery
+  protect_from_forgery
   helper_method :current_user
   def current_user
     @current_user ||= User.find(session[:uid]) if session[:uid]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
     def new
         if(current_user != nil)
             redirect_to '/'
@@ -9,7 +8,6 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_email(params[:user][:email])
         if user && user.authenticate(params[:user][:password])
-            # cookies.permanent.signed[:id] = user.id
             session[:uid] = user.id
             redirect_to '/'
         else
@@ -19,7 +17,7 @@ class SessionsController < ApplicationController
     end
     
     def destroy
-        session[:id] = nil
+        session[:uid] = nil
         redirect_to '/login'
     end
 end
