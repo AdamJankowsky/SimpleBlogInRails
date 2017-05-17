d62c69f5a9a3587556be1c05f868f57a7fa98cf2
Adam Jankowski <adam.jankowski@protonmail.com>
2017-05-17 22:38:12 +0200

    Added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507225904) do
+ActiveRecord::Schema.define(version: 20170517203704) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170507225904) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "date"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
