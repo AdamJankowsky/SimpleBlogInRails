ca738b381f1ba4371263c205e055d96f7797ce00
Adam Jankowski <adam1jankowski@gmail.com>
2017-06-02 21:42:24 +0200

    Added some validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
 
+
+
+    validates :name, :email, presence: true
+    validates :email, uniqueness: true
     has_secure_password
+    validates :password, length: { minimum: 6 }
 
 end
